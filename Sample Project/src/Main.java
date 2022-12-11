49c0c1665a219295abc13db6ad4dfc1cce96bcb7
Alden Karlyle Lim Brillantes <aldenkarlyle.brillantes@benilde.edu.ph>
2022-12-11 13:28:47 +0300

    Added Sample Project

---
+++ Sample Project/src/Main.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        //System.out.println("Hello world!");
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("what is ur name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+
+
+    }
+}
